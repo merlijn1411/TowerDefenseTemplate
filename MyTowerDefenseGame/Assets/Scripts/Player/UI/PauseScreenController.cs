871a839f2dcac4a35f944c308ed4889fc9d1f905
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2023-10-25 19:31:33 +0200

    added PauseScreen

---
+++ MyTowerDefenseGame/Assets/Scripts/Player/UI/PauseScreenController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseScreenController : MonoBehaviour
+{
+    public Canvas _PauseScreen;
+    public static bool GameisPaused = false;
+    void Start()
+    {
+        Resume();    
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Pause()
+    {
+        _PauseScreen.enabled = true;
+        GameisPaused = true;
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        _PauseScreen.enabled = false;
+        GameisPaused = false;
+        Time.timeScale = 1f;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
