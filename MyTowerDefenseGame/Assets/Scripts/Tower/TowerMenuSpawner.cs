118cb61f9ab782c020fa6fbf268bc307bd79cd5d
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-08-22 14:43:05 +0200

    implemented cost in script

--- MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuSpawner.cs
+++ MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuSpawner.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 public class TowerMenuSpawner : MonoBehaviour
 {
     [SerializeField] private List<TowerData> towerList;
+    [SerializeField] private GameObject menuParent;
     private Button[] _menuButtons;
-    
     
     private void Start()
     {
@@ -17,15 +17,30 @@ public class TowerMenuSpawner : MonoBehaviour
     {
         _menuButtons = GetComponentsInChildren<Button>();
         
-        _menuButtons[0].onClick.AddListener(() => SetMenuData(towerList[0].TowerPrefab));
-        _menuButtons[1].onClick.AddListener(() => SetMenuData(towerList[1].TowerPrefab));
+        _menuButtons[0].onClick.AddListener(() => SetTowerData(towerList[0].TowerPrefab, 0));
+        _menuButtons[1].onClick.AddListener(() => SetTowerData(towerList[1].TowerPrefab, 1));
     }
 
-    private void SetMenuData(GameObject tower)
+    private void SetTowerData(GameObject tower, int towerIndex)
     {
+        if (!CheckBalance(towerIndex)) return;
         var position = gameObject.transform.position;
         var rotation = gameObject.transform.rotation;
         
         Instantiate(tower, position, rotation);
+        Destroy(menuParent);
+
+    }
+
+    private bool CheckBalance(int towerIndex)
+    {
+        if (PlayerStats.Money < towerList[towerIndex].Cost)
+        {
+            Debug.Log("Can't Afford Building");
+            return false;
+        }
+        
+        PlayerStats.Money -= towerList[towerIndex].Cost;
+        return true;
     }
 }
