35eedeea8e5857d676830dc44ae01f66300a1723
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-08-22 14:10:07 +0200

    Instantiating works

--- MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuSpawner.cs
+++ MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuSpawner.cs
@@ -6,6 +6,8 @@ public class TowerMenuSpawner : MonoBehaviour
 {
     [SerializeField] private List<TowerData> towerList;
     private Button[] _menuButtons;
+    
+    
     private void Start()
     {
         InizializeButtons();
@@ -15,12 +17,15 @@ public class TowerMenuSpawner : MonoBehaviour
     {
         _menuButtons = GetComponentsInChildren<Button>();
         
-        _menuButtons[0].onClick.AddListener(SetMenuData(towerList[0].TowerPrefab, 0);
-        _menuButtons[1].onClick.AddListener(SetMenuData(towerList[1].TowerPrefab,1));
+        _menuButtons[0].onClick.AddListener(() => SetMenuData(towerList[0].TowerPrefab));
+        _menuButtons[1].onClick.AddListener(() => SetMenuData(towerList[1].TowerPrefab));
     }
 
-    private void SetMenuData(GameObject tower,int towerIndex)
+    private void SetMenuData(GameObject tower)
     {
+        var position = gameObject.transform.position;
+        var rotation = gameObject.transform.rotation;
         
+        Instantiate(tower, position, rotation);
     }
 }
