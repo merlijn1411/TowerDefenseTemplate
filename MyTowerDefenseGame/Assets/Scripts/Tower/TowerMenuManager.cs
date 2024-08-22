f6e0d82515f6c92113c460c2677196a5d948cbb1
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-08-22 14:51:03 +0200

    renamed script and removed old files

--- MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuManager.cs
+++ MyTowerDefenseGame/Assets/Scripts/Tower/TowerMenuManager.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TowerMenuSpawner : MonoBehaviour
+public class TowerMenuManager : MonoBehaviour
 {
     [SerializeField] private List<TowerData> towerList;
     [SerializeField] private GameObject menuParent;
